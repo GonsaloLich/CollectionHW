7be5171cd45d1adee20307e601968555158538eb
GonsaloLich <kantsaris@gmail.com>
2024-01-14 17:57:56 +0300

    Initial commit

---
+++ src/main/java/com/example/collectionhw/CollectionHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectionhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionHwApplication.class, args);
+	}
+
+}
